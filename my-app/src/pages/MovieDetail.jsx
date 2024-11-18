19f02186648537ae55185f3fea5d222db622cd77
apricity2u <gkdms9849@gmail.com>
2024-11-18 14:35:00 +0900

    [feat] bulid MovieDetail structure

--- my-app/src/pages/MovieDetail.jsx
+++ my-app/src/pages/MovieDetail.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function MovieDetail() {
   return (
-    <div>MovieDetail</div>
+    <>
+      <h2>MovieDetail</h2>
+      <div>
+        <img src="" alt="영화포스터" />
+        <div>
+          <h3>영화제목</h3>
+          <button>관심</button>
+        </div>
+        <div>
+          <h4>상세정보</h4>
+          <p>상제정보디테일</p>
+        </div>
+        <div>
+          <h4>후기</h4>
+          <p>후기디테일</p>
+        </div>
+      </div>
+    </>
   )
 }
