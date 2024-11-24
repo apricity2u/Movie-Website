6df126791613a3184b9b59db5b147296b5d73667
apricity2u <gkdms9849@gmail.com>
2024-11-25 01:03:46 +0900

    [feat] reset movie search data when click reset button

--- my-app/src/Components/RootHeaderSearch.jsx
+++ my-app/src/Components/RootHeaderSearch.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import React, { useRef, useState } from "react";
 import movieApi from "../api/movieApi";
 import MovieSimpleDetail from "./MovieSimpleDetail";
 import Button from "./Button";
@@ -8,6 +6,13 @@ import Button from "./Button";
 export default function RootHeaderSearch() {
   const [movieFormData, setMovieFormData] = useState();
   const [movieInput, setMovieInput] = useState();
+  const formRef = useRef();
+
+  function handleSearchReset() {
+    setMovieInput("");
+    formRef.current.reset();
+    setMovieFormData("");
+  }
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -18,22 +23,35 @@ export default function RootHeaderSearch() {
 
   function handleMovieInput(e) {
     setMovieInput(e.target.value);
+    console.log(movieInput)
   }
 
   return (
     <div className="movie-search-bar">
-      <form onSubmit={handleSubmit} className="movie-search-style">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search movie what you want !"
-          onChange={handleMovieInput}
-        />
-        <Button>Search!</Button>
+      <form
+        onSubmit={handleSubmit}
+        ref={formRef}
+        className="movie-search-style"
+      >
+        <div className="flex gap">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search movie what you want !"
+            onInput={handleMovieInput}
+            required
+          />
+          {movieFormData && (
+            <button onClick={handleSearchReset} className="reset-button">
+              X
+            </button>
+          )}
+        </div>
+        <Button>Search</Button>
       </form>
       {movieFormData && (
-        <ul className="movie-category-style">
+        <ul className="movie-category-style margin">
           {movieFormData.slice(0, 5).map((movie) => {
             const { id, title, poster_path } = movie;
 
